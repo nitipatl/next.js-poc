0dded524ff9080af1b54667123ff4414babae7ee
Nitipat L <iamsvz@gmail.com>
2017-08-13 17:37:01 +0700

    add index to storybook

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../app/components/Header'
+import Index from '../pages/index'
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, object } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
+
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -12,5 +15,19 @@ storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
-storiesOf('Layout', module)
-  .add('Header', () => <Header title="ทดสอบ" /> )
+
+const stories = storiesOf('Layout', module)
+stories.addDecorator(withKnobs)
+stories.add('Header', () => {
+    const title = text('title', 'ทดสอบ')
+    return (<Header title={title} />)
+  }
+)
+stories.add('Index', () => {
+    const shows = [
+      {show: { id: 1, name: 'test'}},
+      {show: {id: 2, name: 'test2'}}
+    ]
+    return (<Index shows={object('shows', shows)} />)
+  }
+)
