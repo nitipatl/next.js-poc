2e2ba461500ee6b70eedc8e8233535c5907a448a
Nitipat L <iamsvz@gmail.com>
2017-08-14 23:39:00 +0700

    add experiments

--- stories/__tests__/Storyshot.test.js
+++ stories/__tests__/Storyshot.test.js
@@ -1,5 +1,6 @@
 import initStoryshots, { shallowSnapshot } from '@storybook/addon-storyshots';
 import shallowRenderer from 'react-test-renderer/shallow'
+import Header from '../../app/components/Header'
 
 initStoryshots({
     // test: shallowSnapshot
@@ -8,4 +9,12 @@ initStoryshots({
     //     const result = renderer.render(story.render(context))
     //     expect(result).toMatchSnapshot()
     // }
+    test: ({ story, context }) => {
+        // console.log(story.render(context).props.initialContent)
+        const shallow = new shallowRenderer
+        // const code = <Header title="OK" />
+        // console.log(code)
+        shallow.render(story.render(context).props.initialContent)
+        expect(shallow.getRenderOutput()).toMatchSnapshot();
+    }
 });
